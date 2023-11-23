5cb7d64ebad82fc9b1ea722ab19bcd37b27c6649
wissam-alshohof <wissam.alshohof@gmail.com>
2023-11-23 17:33:43 +0400

    firebase implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth, AuthModule } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,12 @@ import { SharedModule } from './shared/shared.module';
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
-    SharedModule
+    SharedModule,
+    AuthModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"car-inspection-portal","appId":"1:128370555043:web:8d246b3324ab0b7628a716","databaseURL":"https://car-inspection-portal-default-rtdb.asia-southeast1.firebasedatabase.app","storageBucket":"car-inspection-portal.appspot.com","apiKey":"AIzaSyBsLK5umOujc5DfzLzKUcJvotnrIDJr8eA","authDomain":"car-inspection-portal.firebaseapp.com","messagingSenderId":"128370555043"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase())
   ],
   providers: [],
   bootstrap: [AppComponent]
